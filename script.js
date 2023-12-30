eba3a6fb4f6a626592ff5879f768734a5c11f255
Buddhadeb koner <iambuddhdebkoner@gmail.com>
2023-12-30 16:13:24 +0530

    my mind is blowing

--- script.js
+++ script.js
@@ -2,30 +2,33 @@ const projects = [
     // (Projects array remains unchanged)
     {
         image: 'projectDetails/project1.jpg',
-        title: 'Project 1',
-        details: 'This is the details text for Project 1.',
-        skills: ['HTML', 'CSS', 'JS']
+        title: 'Calculater',
+        details: 'An inquisitive Computer Science Engineering student, skilled in leadership, seeking to leverage solid development skills with focus on collaboration, communication and passion. ',
+        skills: ['HTML', 'CSS', 'JS'],
+        link: 'https://github.com/BuddhadebKoner/stylelist-calculater'
     },
     {
         image: 'projectDetails/project2.jpg',
-        title: 'Project 2',
+        title: 'Travel vlog webapp',
         details: 'This is the details text for Project 2.',
-        skills: ['python', 'c++']
+        skills: ['python', 'c++'],
+        link: 'https://github.com/BuddhadebKoner/TravelVlogerWebapp'
     },
     {
         image: 'projectDetails/project3.jpg',
-        title: 'Project 3',
+        title: 'Advance portpoilo',
         details: 'This is the details text for Project 3.',
-        skills: ['HTml', 'c#']
+        skills: ['HTML', 'c#'],
+        link: 'https://github.com/BuddhadebKoner/advance-portpolio'
     },
     // ... (repeat for other projects)
 ];
 
 let currentProjectIndex = 0;
-const projectBox = document.getElementById('project');
+const projectContainer = document.querySelector('.section.worksection');
 
 function showProject(index) {
-    projectBox.innerHTML = getProjectHTML(index);
+    projectContainer.innerHTML = getProjectHTML(index);
     currentProjectIndex = index;
 }
 
@@ -34,29 +37,28 @@ function showNextProject() {
     showProject(currentProjectIndex);
 }
 
-function showPreviousProject() {
-    currentProjectIndex = (currentProjectIndex - 1 + projects.length) % projects.length;
-    showProject(currentProjectIndex);
-}
-
 function getProjectHTML(index) {
     const project = projects[index];
     const skillsHTML = project.skills.map(skill => `<li>${skill}</li>`).join('');
     return `
-    <div class="feature-section">
         <div class="project-box" id="project">
+        <h3 id="worksectionheading">Works</h3>
+        <p class="DetailsInAboutSectionHeading3">${project.title}</p>
+        <img src="img/laptopFream.png" alt="" class="laptopFream">
             <img src="${project.image}" alt="${project.title}" class="project-image">
+            <button class="swap-button" onclick="showNextProject()"><img src="svgItems/Frame.svg" alt=""></button>
         </div>
+        
         <div class="project-details">
             <h2>${project.title}</h2>
-            <p>${project.details}</p>
-            <p>Skills:</p>
+            <p class="DetailsInAboutSectionHeading3">${project.details}</p>
             <ul>${skillsHTML}</ul>
+            <a href="${project.link}" target="_blank"><a href="#" target="_blank">
+            <img src="icons/github.svg" alt="">
+        </a></a>
         </div>
-    </div>
     `;
 }
-
 
 // Initial project display
 showProject(currentProjectIndex);
