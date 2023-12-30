57a56e161178c90d23d7a5736a5f6e3d205a4359
Buddhadeb koner <iambuddhdebkoner@gmail.com>
2023-12-30 23:32:55 +0530

    my mind is blowing

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ const projects = [
     // (Projects array remains unchanged)
     {
         image: 'projectDetails/project2.jpg',
-        title: 'Travel vlog webapp',
+        title: '1. Travel vlog webapp',
         details: 'This is the details text for Project 2.',
         skills: ['python', 'c++'],
         link: 'https://github.com/BuddhadebKoner/TravelVlogerWebapp'
     },
     {
         image: 'projectDetails/project1.jpg',
-        title: 'Calculater',
+        title: '2. Calculater',
         details: 'An inquisitive Computer Science Engineering student, skilled in leadership, seeking to leverage solid development skills with focus on collaboration, communication and passion. ',
         skills: ['HTML', 'CSS', 'JS'],
         link: 'https://github.com/BuddhadebKoner/stylelist-calculater'
     },
     {
         image: 'projectDetails/project3.jpg',
-        title: 'Advance portpoilo',
+        title: '3. Advance portpoilo',
         details: 'This is the details text for Project 3.',
         skills: ['HTML', 'c#'],
         link: 'https://github.com/BuddhadebKoner/advance-portpolio'
@@ -53,9 +53,9 @@ function getProjectHTML(index) {
             <h2>${project.title}</h2>
             <p class="DetailsInAboutSectionHeading3">${project.details}</p>
             <ul>${skillsHTML}</ul>
-            <a href="${project.link}" target="_blank"><a href="#" target="_blank">
+            <a href="${project.link}" target="_blank">
             <img src="icons/github.svg" alt="" class="githubiconworksection">
-        </a></a>
+            </a>
         </div>
     `;
 }
